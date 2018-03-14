b171db701ed2c75f949888115e99f9202c831a88
city0409 <250924125@qq.com>
2018-03-14 10:02:23 +0800

    Initial commit

---
+++ Assets/Editor/NewBehaviourScriptEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(NewBehaviourScript))]
+public class NewBehaviourScriptEditor : Editor 
+{
+
+	public void OnSceneGUI() 
+	{
+        NewBehaviourScript t = (NewBehaviourScript)target;
+
+        for (int i = 0; i < t.path.Count; i++)
+        {
+            EditorGUI.BeginChangeCheck();
+
+            Vector3 newPoint = UnityEditor.Handles.FreeMoveHandle(t.path[i], Quaternion.identity, 0.5f, new Vector3(.25f, .25f, .25f), Handles.CircleHandleCap);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(target, "Free Move Handle");
+                t.path[i] = newPoint;
+            }
+        }
+	}
+}
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour 
+{
+    public List<Vector3> path = new List<Vector3>();
+
+	private void Awake() 
+	{
+		
+	}
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos () 
+	{
+        for (int i = 0; i < path.Count; i++)
+        {
+            Gizmos.DrawWireSphere(path[i], 0.2f);
+        }
+	}
+#endif
+}
